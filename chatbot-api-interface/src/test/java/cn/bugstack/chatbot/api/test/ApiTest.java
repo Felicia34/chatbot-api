d8a27fa663a268da419e691c4863c11a5af65bc8
Felicia34 <624389866@qq.com>
2024-04-06 20:19:40 +0800

    TanyaXxx,feat:second ddd project

--- chatbot-api-interface/src/test/java/cn/bugstack/chatbot/api/test/ApiTest.java
+++ chatbot-api-interface/src/test/java/cn/bugstack/chatbot/api/test/ApiTest.java
@@ -1,7 +1,69 @@
 package cn.bugstack.chatbot.api.test;
 
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+
+
+import java.io.IOException;
+
 /*
     单元测试
  */
+
+
 public class ApiTest {
+
+    @Test
+    public void query_unanswered_questions() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        HttpGet get = new HttpGet("https://api.zsxq.com/v2/groups/88888842252812/topics?scope=unanswered_questions&count=20");
+
+        get.addHeader("cookie","zsxq_access_token=5A827CDD-8307-3BF5-3106-34828575FDDB_9D13A8559933B7A9; zsxqsessionid=b1e2f8416fc4fd0f455c3d8abc7ee9c2; abtest_env=beta");
+        get.addHeader("Content-Type","application/json;charset=utf8");
+
+        CloseableHttpResponse response = httpClient.execute(get);
+        if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        }else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+    }
+
+    @Test
+    public void answer() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        HttpPost post = new HttpPost("https://api.zsxq.com/v2/topics/8855811854814112/answer");
+
+        post.addHeader("cookie","zsxq_access_token=5A827CDD-8307-3BF5-3106-34828575FDDB_9D13A8559933B7A9; zsxqsessionid=b1e2f8416fc4fd0f455c3d8abc7ee9c2; abtest_env=beta");
+        post.addHeader("Content-Type","application/json;charset=utf8");
+
+        String paramJson = "{\n" +
+                "  \"req_data\": {\n" +
+                "    \"text\": \"我也不会！\\n\",\n" +
+                "    \"image_ids\": [],\n" +
+                "    \"silenced\": false\n" +
+                "  }\n" +
+                "}";
+
+        StringEntity stringEntity = new StringEntity(paramJson, ContentType.create("text/json", "UTF-8"));
+        post.setEntity(stringEntity);
+
+        CloseableHttpResponse response = httpClient.execute(post);
+        if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        }else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+
+    }
 }
